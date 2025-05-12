715b851c4871a9eb422772ece93708692caeb62a
Vs Code <abhishek29073@gmail.com>
2025-05-12 18:54:15 +0530

    update

--- Assignment 20/question_10.cpp
+++ Assignment 20/question_10.cpp
@@ -1,11 +1,22 @@
 // 10. Write a program to print a string in reverse using a pointer
 #include<iostream>
 using namespace std;
+void revers(char *str)
+{
+    if(*(str)=='\0')
+      return;
+    revers(str+1);
+    cout<<str[0];
+  
+}
 int main()
 {
     unsigned int size =50;
     char str[size];
     char *ptr = str;
+    cout<<"Enter string: ";
     cin.getline(ptr,size);
+    cout<<"Reverse string: ";
+  revers(ptr);
     return 0;
 }
